6c7ebfed309d35a289534c005ce8c7d78f84bf20
Agustin <agustin@ensolvers.com>
2020-07-27 23:41:20 -0300

    first commit project

---
+++ miprimeraapp/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React   from 'react';
+import { View, Text } from 'react-native';
+
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <View><Text>hola</Text></View>
+    )
+  }
+}
+
+export default App;
---
+++ readme.txt
@@ -0,0 +1,8 @@
+intalar npm (no de package manager)
+instalar node
+instalate android studio
+///// se hacen siempre
+abris android un emulador
+npm start (en una ventana)
+npm run android (en otra consola)
+
